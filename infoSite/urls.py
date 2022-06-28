6af84d600e4853f5fd4b39dccc89d94058a65f50
Max Trapido <maxtrapido@gmail.com>
2022-06-27 22:12:21 -0700

    Style the headers

--- infoSite/urls.py
+++ infoSite/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path("", views.index),
-	path("about-us", views.aboutus)
+	path("about-us", views.aboutus),
+	path("classes", views.classes),
+	path("contact-us", views.contactus)
+
 ]
--- summerCourses/urls.py
+++ summerCourses/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', include("infoSite.urls"))
+	path('', include("infoSite.urls")),
+
 ]
