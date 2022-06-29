7ca61f6017f7df5344cc0542b62c57dd5bee0c00
Timofei Kachan <67656268+TimofeiK27@users.noreply.github.com>
2022-06-29 16:41:54 -0700

    starting implementing responsive class page

--- infoSite/static/infoSite/classes.js
+++ infoSite/static/infoSite/classes.js
@@ -1,1 +1,7 @@
-console.log('hi');
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('#PremierePro').addEventListener("click", ()=>{
+	    document.querySelector('#PremierePro').innerHTML = "  <p>Superpower Premiere Pro Open to all Middle Schoolers July 18th-July 22nd 5:00-7:00 pm Have you ever wanted to be a superhero? With Premiere Pro you can be! In this course you will learn the basics of Premiere Pro, including keyframes, masking, and special effects. In addition to learning about the digital side of filmmaking, we will also explore the practical side, using miniatures, gravity, and green screens to our advantage. The course will culminate with you making a visual effects shot with any superpower of your choosing. </p>";
+	    console.log('hi');
+    })
+    console.log('bye');
+});
