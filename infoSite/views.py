c9fbe50f91f276d7cc83d1c61a25a8e02b580a57
Max Trapido <maxtrapido@gmail.com>
2022-07-06 17:04:08 -0700

    delete three classes on class page

--- infoSite/views.py
+++ infoSite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import reverse
 from . import templates
-
+from django.http import JsonResponse
 
 # Create your views here.
 
