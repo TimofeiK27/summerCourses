412e724053d6bfe740316f88664f1f1bf9d1087c
Max Trapido <maxtrapido@gmail.com>
2022-07-14 16:03:23 -0700

    translations in early stages

--- infoSite/static/scripts/languages.js
+++ infoSite/static/scripts/languages.js
@@ -1,26 +1,24 @@
 let langdata = require('./translations.json');
+console.log(langdata);
 
 //apply the language values to the content
 document.addEventListener('DOMContentLoaded', () => {
 
+    console.log('asfsa');
+    localStorage.setItem('lang', 'en');
     let zones = document.querySelectorAll('[data-key]');
     applyStrings(zones);
 });
 
-function applyStrings(containers) {
-    let locale =
-    containers.forEach(container => {
-        //find all the elements that have data-key
-        let locale = container.getAttribute('lang');
-        //console.log('looking inside of ', locale);
-        container.querySelectorAll(`[data-key]`).forEach(element => {
-            let key = element.getAttribute('data-key');
-            //console.log(element);
-            //console.log(key);
-            let lang = locale.substr(0, 2); //first 2 characters
-            if (key) {
-                element.textContent = langdata.languages[lang].strings[key];
-            }
-        });
-    })
+function applyStrings(elements) {
+    let locale = localStorage.getItem('lang');
+    elements.forEach(element => {
+        let key = element.getAttribute('data-key');
+        //console.log(element);
+        //console.log(key);
+        /*let lang = locale.substr(0, 2); //first 2 characters*/
+        if (key) {
+            element.textContent = langdata.languages[lang].strings[key];
+        }
+    });
 }
