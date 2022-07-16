a5769ee869c9f0f46b5cc98fa04a1bf9eb3ad5c2
Max Trapido <maxtrapido@gmail.com>
2022-07-15 18:00:44 -0700

    make table items link to corresponding class

--- infoSite/static/scripts/calendar.js
+++ infoSite/static/scripts/calendar.js
@@ -1,3 +1,16 @@
+const CODING = `<a href="/classes#coding"><div class="classtimeentry compsci">
+                    <span><strong>Coding:</strong></span><br>
+                    <span>12:00 PM — 1:20 PM</span>
+                </div></a>`;
+const FILM_MAKING = `<a href="/classes#film-making"><div class="classtimeentry moviemake">
+                        <span><strong>Movie Making:</strong></span><br>
+                        <span>1:30 PM — 2:50 PM</span>
+                     </div></a>`;
+const ART = `<a href="/classes#art"><div class="classtimeentry drawing">
+                 <span><strong>Drawing/Cartooning:</strong></span><br>
+                 <span>3:00 PM — 4:20 PM</span>
+             </div></a>`;
+
 class Calendar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -14,119 +27,65 @@ class Calendar extends HTMLElement {
                 <tbody>
                     <tr>
                         <td>
-                            <span class="dateofclass">July <strong>18<sup>th</sup></strong></span>
-                            <a href="#comp-sci-description"><div class="classtimeentry compsci">
-                                <span><strong>Computer Science:</strong></span><br>
-                                <span>12:00 PM — 1:20 PM</span>
-                            </div></a>
-                            <a href="#film-making-description"><div class="classtimeentry moviemake">
-                                <span><strong>Movie Making:</strong></span><br>
-                                <span>1:30 PM — 2:50 PM</span>
-                            </div></a>
-                            <a href="#drawing-description"><div class="classtimeentry drawing">
-                                <span><strong>Drawing/Cartooning:</strong></span><br>
-                                <span>3:00 PM — 4:20 PM</span>
-                            </div></a>
+                            <p class="classdate">July <strong>18<sup>th</sup></strong></p>
+                            ${CODING}
+                            ${FILM_MAKING}
+                            ${ART}
                         </td>
                         <td>
-                            July <strong>19<sup>th</sup></strong>
+                            <p class="classdate">July <strong>19<sup>th</sup></strong></p>
                         </td>
                         <td>
-                            July <strong>20<sup>th</sup></strong>
-                            <div class="classtimeentry compsci">
-                                <span><strong>Computer Science:</strong></span><br>
-                                <span>12:00 PM — 1:20 PM</span>
-                            </div>
-                            <div class="classtimeentry moviemake">
-                                <span><strong>Movie Making:</strong></span><br>
-                                <span>1:30 PM — 2:50 PM</span>
-                            </div>
-                            <div class="classtimeentry drawing">
-                                <span><strong>Drawing/Cartooning:</strong></span><br>
-                                <span>3:00 PM — 4:20 PM</span>
-                            </div>
+                            <p class="classdate">July <strong>20<sup>th</sup></strong><p>
+                            ${CODING}
+                            ${FILM_MAKING}
+                            ${ART}
                         </td>
                         <td>
-                            July <strong>21<sup>st</sup></strong>
+                            <p class="classdate">July <strong>21<sup>st</sup></strong></p>
                         </td>
                         <td>
-                            July <strong>22<sup>nd</sup></strong>
+                            <p class="classdate">July <strong>22<sup>nd</sup></strong></p>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <span class="dateofclass">July <strong>25<sup>th</sup></strong></span>
-                            <div class="classtimeentry compsci">
-                                <span><strong>Computer Science:</strong></span><br>
-                                <span>12:00 PM — 1:20 PM</span>
-                            </div>
-                            <div class="classtimeentry moviemake">
-                                <span><strong>Movie Making:</strong></span><br>
-                                <span>1:30 PM — 2:50 PM</span>
-                            </div>
-                            <div class="classtimeentry drawing">
-                                <span><strong>Drawing/Cartooning:</strong></span><br>
-                                <span>3:00 PM — 4:20 PM</span>
-                            </div>
+                            <p class="classdate">July <strong>25<sup>th</sup></strong></p>
+                            ${CODING}
+                            ${FILM_MAKING}
+                            ${ART}
                         </td>
                         <td>
-                            July <strong>26<sup>th</sup></strong>
+                            <p class="classdate">July <strong>26<sup>th</sup></strong></p>
                         </td>
                         <td>
-                            July <strong>27<sup>th</sup></strong>
-                            <div class="classtimeentry compsci">
-                                <span><strong>Computer Science:</strong></span><br>
-                                <span>12:00 PM — 1:20 PM</span>
-                            </div>
-                            <div class="classtimeentry moviemake">
-                                <span><strong>Movie Making:</strong></span><br>
-                                <span>1:30 PM — 2:50 PM</span>
-                            </div>
-                            <div class="classtimeentry drawing">
-                                <span><strong>Drawing/Cartooning:</strong></span><br>
-                                <span>3:00 PM — 4:20 PM</span>
-                            </div>
+                            <p class="classdate">July <strong>27<sup>th</sup></strong></p>
+                            ${CODING}
+                            ${FILM_MAKING}
+                            ${ART}
                         </td>
                         <td>
-                            July <strong>28<sup>th</sup></strong>
+                            <p class="classdate">July <strong>28<sup>th</sup></strong></p>
                         </td>
                         <td>
-                            July <strong>29<sup>th</sup></strong>
+                            <p class="classdate">July <strong>29<sup>th</sup></strong></p>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <span class="dateofclass">August <strong>1<sup>st</sup></strong></span>
-                            <div class="classtimeentry compsci">
-                                <span><strong>Computer Science:</strong></span><br>
-                                <span>12:00 PM — 1:20 PM</span>
-                            </div>
-                            <div class="classtimeentry moviemake">
-                                <span><strong>Movie Making:</strong></span><br>
-                                <span>1:30 PM — 2:50 PM</span>
-                            </div>
-                            <div class="classtimeentry drawing">
-                                <span><strong>Drawing/Cartooning:</strong></span><br>
-                                <span>3:00 PM — 4:20 PM</span>
-                            </div>
+                            <p class="classdate">August <strong>1<sup>st</sup></strong></p>
+                            ${CODING}
+                            ${FILM_MAKING}
+                            ${ART}
                         </td>
                         <td>
-                            August <strong>2<sup>nd</sup></strong>
+                            <p class="classdate">August <strong>2<sup>nd</sup></strong></p>
                         </td>
                         <td>
-                            August <strong>3<sup>rd</sup></strong>
-                            <div class="classtimeentry compsci">
-                                <span><strong>Computer Science:</strong></span><br>
-                                <span>12:00 PM — 1:20 PM</span>
-                            </div>
-                            <div class="classtimeentry moviemake">
-                                <span><strong>Movie Making:</strong></span><br>
-                                <span>1:30 PM — 2:50 PM</span>
-                            </div>
-                            <div class="classtimeentry drawing">
-                                <span><strong>Drawing/Cartooning:</strong></span><br>
-                                <span>3:00 PM — 4:20 PM</span>
-                            </div>
+                            <p class="classdate">August <strong>3<sup>rd</sup></strong></p>
+                            ${CODING}
+                            ${FILM_MAKING}
+                            ${ART}
                         </td>
                         <td></td>
                         <td></td>
